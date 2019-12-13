2186bf8d4afb91236fbd9fa98012dc76e448bd37
prabhavagrawal <41739517+prabhavagrawal@users.noreply.github.com>
2019-12-13 13:43:56 +0530

    Update ec2-basics.sh

--- ec2-fundamentals/ec2-basics.sh
+++ ec2-fundamentals/ec2-basics.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-########################################################
-##### USE THIS FILE IF YOU LAUNCHED AMAZON LINUX 2 #####
-########################################################
-
-# get admin privileges
 sudo su
-
-# install httpd (Linux 2 version)
 yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
